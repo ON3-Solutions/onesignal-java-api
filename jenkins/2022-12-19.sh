911dfe0096ea0e254dd75849e9f178b44ba94322
Jesus Saad <jesus.saad1@gmail.com>
2023-07-03 15:23:46 -0300

    fix: corrigindo o script do Jenkins

--- jenkins/2022-12-19.sh
+++ jenkins/2022-12-19.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+/Users/Shared/ON3/git/devops/github/add_web_hook.sh ON3-Solutions onesignal-java-api
 /Users/Shared/ON3/git/devops/jenkins/api/script_clone_default_build_job.sh 'onesignal-java-api' 'Infra'
